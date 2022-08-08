6ad762e4e15751f890eb4ad1598ea2c389c63860
romaanbepari <105905384+romaanbepar@users.noreply.github.com>
2022-08-08 10:18:56 +0530

    dfirst

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import Rating from "./Rating";
+
+function App() {
+
+  
+  return (
+    <div className="App">
+      <h1> star rating problem</h1>
+    <Rating/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Rating.js
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+
+import { FaStar } from "react-icons/fa";
+const Rating = () => {
+  const [rating, setRating] = useState(null);
+  const [hover, setHover] = useState(null);
+  return (
+    <div>
+      {[...Array(5)].map((star, i) => {
+        const ratingValue = i + 1;
+        return (
+          
+          <label>
+            <input
+              type="radio"
+              name="rating"
+              value={ratingValue}
+              onClick={() => setRating(ratingValue)}
+            />
+            <FaStar
+              size={100}
+              className="star"
+              color={ ratingValue <= (hover || rating) ? "yellow" : "black"}
+              onMouseEnter={() => setHover(ratingValue)}
+              onMouseLeave={() => setHover(null)}
+            />
+          </label>
+        );
+      })}
+      <p>the rating is{rating}</p>
+    </div>
+  );
+};
+
+export default Rating;
